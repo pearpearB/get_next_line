a0b0b11d36f5ab26c5dc10435191b5e0756431b3
pearpearB <smilyoung@gmail.com>
2022-01-24 20:09:35 +0900

    read 함수 공부

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,22 @@
 /*   By: jabae <jabae@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 15:59:14 by jabae             #+#    #+#             */
-/*   Updated: 2022/01/23 22:57:08 by jabae            ###   ########.fr       */
+/*   Updated: 2022/01/24 20:07:44 by jabae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h> // 제출 전 꼭 지우기!
 
 char	*get_next_line(int fd)
 {
-	char buffer[BUFFER_SIZE + 1];
-	
-	printf("%d\n", fd);
+	static char buffer[BUFFER_SIZE + 1];
 
-	if (fd < 0 || read(fd, buffer, BUFFER_SIZE) == -1 || BUFFER_SIZE <= 0)
+	if (fd < 0  || BUFFER_SIZE <= 0)
 		return (NULL);
 
-	printf("%s\n", buffer);
-
-	return 0;
+	while(read(fd, buffer, BUFFER_SIZE) > 0)
+	{
+		return (buffer);
+	}
+	return (NULL);
 } 
--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <stdio.h>
 
 int main()
 {
-	get_next_line(open("test.txt", O_RDONLY));
+	int fd = open("test.txt", O_RDONLY);
+
+	while(get_next_line(fd))
+	{
+		printf("%s", get_next_line(fd));
+	}
 	
 	return 0;
 }
