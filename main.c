c87c32750fd8e37d82d0e5fbd18665e2d725d3a6
pearpearB <smilyoung@gmail.com>
2022-04-21 18:38:24 +0900

    bonus 메인문 추가

--- main.c
+++ main.c
@@ -2,28 +2,58 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+// int main()
+// {
+// 	int		fd, line;
+// 	char	*ret;
+
+// 	fd = open("test_file1.txt", O_RDONLY);
+// 	line = 0;
+
+// 	// close(fd); // unvalid fd 일 때,
+
+// 	while((ret = get_next_line(fd)) > 0)
+// 	{
+// 		printf(">%d : %s", line, ret);
+// 		free(ret);
+// 		line++;
+// 	}
+// 	printf("\n->%d : %s", line, ret); // 마지막 (NULL) 출력
+// 	free(ret);
+
+// 	close(fd);
+
+// 	return 0;
+// }
+
+// gcc -Wall -Wextra -Werror -D BUFFER_SIZE=42 ./get_next_line.c ./get_next_line_utils.c ./main.c
+// lldb : -g -fsanitize=address
+
 int main()
 {
-	int		fd, line;
+	int		fd[3], fd_idx, line;
 	char	*ret;
 
-	fd = open("test.txt", O_RDONLY);
+	fd[0] = open("test_file1.txt", O_RDONLY);
+	fd[1] = open("test_file2.txt", O_RDONLY);
+	fd[2] = open("test_file3.txt", O_RDONLY);
 
-	// close(fd); // unvalid fd 일 때,
-
-	while((ret = get_next_line(fd)) > 0)
+	fd_idx = 0;
+	while(fd_idx < 3)
 	{
-		printf(">%d : %s", line, ret);
+		line = 0;
+		while((ret = get_next_line(fd[fd_idx])) > 0)
+		{
+			printf(">%d : %s", line, ret);
+			free(ret);
+			line++;
+		}
+		printf("\n->%d : %s\n", line, ret); // 마지막 (NULL) 출력
 		free(ret);
-		line++;
+		close(fd[fd_idx]);
+		fd_idx++;
 	}
-	printf("\n->%d : %s", line, ret); // (NULL)
-	free(ret);
-
-	close(fd);
-
 	return 0;
 }
 
-// gcc -Wall -Wextra -Werror -D BUFFER_SIZE=42 ./*.c
-// lldb : -g -fsanitize=address
+// gcc -Wall -Wextra -Werror -D BUFFER_SIZE=42 ./get_next_line_bonus.c ./get_next_line_utils_bonus.c ./main.c
